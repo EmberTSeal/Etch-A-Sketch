953b07c60ed5266c9bcc290b1f8340cc7ddfe53e
EmberTSeal <trishaseal49@gmail.com>
2023-06-27 15:08:53 +0530

    add colour function

--- script.js
+++ script.js
@@ -2,17 +2,40 @@ const container = document.querySelector('.container');
 
 for (let i = 0; i < 16; i++) {
     row = document.createElement('div');
-    row.style.height = '21px';
+    row.style.height = '20px';
     row.style.display = 'flex';
     row.style.margin = '0'
+    row.style.border = 'none'
     for (let j = 0; j < 16; j++) {
         const div = document.createElement('div');
+        div.classList.add('grid');
         div.style.margin = '0';
         div.style.width = '20px';
         div.style.height = '20px';
-        div.style.backgroundColor = 'red';
-        div.style.border = '0.5px solid black';
+        div.style.border = '1px solid rgb(0,0,0, 0.2)';
         row.appendChild(div);
     }
     container.appendChild(row);
 }
+
+const grids = document.querySelectorAll('.grid');
+
+let toColour = false;
+
+function paint(){
+    if(toColour === false ){
+        toColour = true;
+        grids.forEach( grid => grid.addEventListener('mousemove', colourGrid));
+    }
+    else{
+        toColour = false;
+        grids.forEach(grid => grid.removeEventListener('mousemove', colourGrid));
+    }
+}
+
+function colourGrid(e){
+    e.target.classList.add('changed');
+}
+
+grids.forEach( grid => grid.addEventListener( 'click', paint));
+
