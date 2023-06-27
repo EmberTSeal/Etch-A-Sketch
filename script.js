dc5bef3500ddfa7c05b78ad10b400fb73587f209
EmberTSeal <trishaseal49@gmail.com>
2023-06-27 17:01:43 +0530

    add grid buttons

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
 const container = document.querySelector('.container');
+const button10 = document.querySelector('#size10');
+const button25 = document.querySelector('#size25');
+const button50 = document.querySelector('#size50');
 
-//creating grids
-for (let i = 0; i < 16; i++) {
-    row = document.createElement('div');
-    row.style.height = '20px';
-    row.style.display = 'flex';
-    row.style.margin = '0'
-    row.style.border = 'none'
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement('div');
-        div.classList.add('grid');
-        div.style.margin = '0';
-        div.style.width = '20px';
-        div.style.height = '20px';
-        row.appendChild(div);
-    }
-    container.appendChild(row);
+//default:
+let gridCount = 10;
+let gridSize = 500 / gridCount;
+createGrid(gridCount, gridSize);
+
+button10.addEventListener('click', function(){
+    clearGrids();
+    gridCount = 10;
+    gridSize = 500/gridCount;
+    createGrid(gridCount, gridSize);
+});
+
+button25.addEventListener('click', function(){
+    clearGrids();
+    gridCount = 25;
+    gridSize = 500/gridCount;
+    createGrid(gridCount, gridSize);
+});
+
+
+button50.addEventListener('click', function(){
+    clearGrids();
+    gridCount = 50;
+    gridSize = 500/gridCount;
+    createGrid(gridCount, gridSize);
+});
+
+//clearing existing grids
+function clearGrids(){
+    container.replaceChildren();
 }
 
+//creating grids
+function createGrid(gridCount, gridSize) {
+    for (let i = 0; i < gridCount; i++) {
+        row = document.createElement('div');
+        row.style.height = `${gridSize}px`;
+        row.style.display = 'flex';
+        row.style.margin = '0'
+        row.style.border = 'none'
+        for (let j = 0; j < gridCount; j++) {
+            const div = document.createElement('div');
+            div.classList.add('grid');
+            div.style.margin = '0';
+            div.style.width = `${gridSize}px`;
+            div.style.height = `${gridSize}px`;
+            div.style.border = '0.2px solid rgb(0,0,0, 0.5)';
+            row.appendChild(div);
+        }
+        container.appendChild(row);
+    }
+}
 const grids = document.querySelectorAll('.grid');
 
 let toColour = false;
@@ -25,7 +62,7 @@ let rainbowMode = false;
 
 let defaultColor = document.querySelector('#colour').value;
 
-grids.forEach( grid => grid.addEventListener( 'click', paint));
+grids.forEach(grid => grid.addEventListener('click', paint));
 
 const clearBtn = document.querySelector('#clear');
 
@@ -35,33 +72,33 @@ clearBtn.addEventListener('click', () => {
 });
 
 const rainbow = document.querySelector('#rainbow');
-rainbow.addEventListener('click', function(){
+rainbow.addEventListener('click', function () {
     rainbowMode = true;
     rainbow.disabled = true;
-    });
+});
 
-function paint(){
-    if(toColour === false ){
+function paint() {
+    if (toColour === false) {
         toColour = true;
-        grids.forEach( grid => grid.addEventListener('mousemove', colourGrid));
+        grids.forEach(grid => grid.addEventListener('mousemove', colourGrid));
     }
-    else{
+    else {
         toColour = false;
         grids.forEach(grid => grid.removeEventListener('mousemove', colourGrid));
     }
 }
 
-function colourGrid(e){
-    if(rainbowMode === false)
+function colourGrid(e) {
+    if (rainbowMode === false)
         e.target.style.backgroundColor = defaultColor;
-    else{
+    else {
         e.target.style.backgroundColor = randomColour();
     }
 }
 
-function randomColour(){
-    let h = Math.floor(Math.random()*360);
+function randomColour() {
+    let h = Math.floor(Math.random() * 360);
     let color = 'hsl(' + h + ', 100%, 50%)';
-    return color; 
+    return color;
 }
 
